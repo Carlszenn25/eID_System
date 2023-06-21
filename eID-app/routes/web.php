7a1c7a9a8ff7235298421c5c8aa442e281b69dcb
Carlszenn25 <chixxbtlog@gmail.com>
2023-06-21 15:56:26 +0800

    Make Changes

--- eID-app/routes/web.php
+++ eID-app/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 })->middleware('guest');
 
 Route::get('/dashboard', function () {
